bcd30edd9250befffd0b45f885633531c439da33
Tudor Tocan <tudor.tocan@gmail.com>
2021-06-21 20:03:33 +0100

    renderSearchResults.js added

--- src/controllers/html/renderSearchResults.js
+++ src/controllers/html/renderSearchResults.js
@@ -1,8 +1,38 @@
-const renderSearchResults = (req, res) => {
+const axios = require("axios");
+
+const renderSearchResults = async (req, res) => {
   const options = {
     layout: "main",
   };
-  res.render("search-results", options);
+  const searchInput = req.query.search;
+
+  const stringifySearch = decodeURI(searchInput);
+
+  console.log(stringifySearch);
+
+  const data = `fields name, cover.url, screenshots.url, rating, multiplayer_modes, release_dates.date, platforms.name, genres.name; search "${stringifySearch}";`;
+  const config = {
+    method: "post",
+    url: "https://api.igdb.com/v4/games",
+    headers: {
+      "Client-ID": "zgvole5aqniz8rnu55rvoxmw3h8d8x",
+      Authorization: "Bearer zzrepo6q4zb5jvw5jxqswmpgc6ef0s",
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+  const response = await axios(config);
+
+  // axios(config)
+  //   .then((res) => console.log(res, "1"))
+  //   .catch((err) => console.log(err));
+
+  console.log(response, "2");
+
+  const results = response.data;
+
+  // res.json(response.data);
+  res.render("searchResults", { results });
 };
 
 module.exports = renderSearchResults;
