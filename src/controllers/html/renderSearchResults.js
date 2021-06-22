51176baa3ec35853c92fa505bbca39330b13f687
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-22 14:34:22 +0100

    resolved issue where "undefined" is rendered when no search query

--- src/controllers/html/renderSearchResults.js
+++ src/controllers/html/renderSearchResults.js
@@ -9,23 +9,27 @@ const renderSearchResults = async (req, res) => {
   const stringifySearch = decodeURI(searchInput);
 
   console.log(stringifySearch);
+  if (searchInput) {
+    const data = `fields name, summary, cover.url, screenshots.url, rating, multiplayer_modes, release_dates.date, platforms.name, genres.name; search "${stringifySearch}";`;
+    const config = {
+      method: "post",
+      url: "https://api.igdb.com/v4/games",
+      headers: {
+        "Client-ID": "zgvole5aqniz8rnu55rvoxmw3h8d8x",
+        Authorization: "Bearer zzrepo6q4zb5jvw5jxqswmpgc6ef0s",
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+    const response = await axios(config);
 
-  const data = `fields name, summary, cover.url, screenshots.url, rating, multiplayer_modes, release_dates.date, platforms.name, genres.name; search "${stringifySearch}";`;
-  const config = {
-    method: "post",
-    url: "https://api.igdb.com/v4/games",
-    headers: {
-      "Client-ID": "zgvole5aqniz8rnu55rvoxmw3h8d8x",
-      Authorization: "Bearer zzrepo6q4zb5jvw5jxqswmpgc6ef0s",
-      "Content-Type": "application/json",
-    },
-    data: data,
-  };
-  const response = await axios(config);
+    const results = response.data;
 
-  const results = response.data;
-
-  res.render("search-results", { options, results });
+    return res.render("search-results", { options, results });
+  } else {
+    const results = null;
+    return res.render("search-results", { options, results });
+  }
 };
 
 module.exports = renderSearchResults;
--- src/controllers/public-api/igdbData.js
+++ src/controllers/public-api/igdbData.js
@@ -7,28 +7,32 @@ const getGameData = async (req, res) => {
 
   console.log(stringifySearch);
 
-  const data = `fields name, cover.url, screenshots.url, rating, multiplayer_modes, release_dates.date, platforms.name, genres.name; search "${stringifySearch}";`;
-  const config = {
-    method: "post",
-    url: "https://api.igdb.com/v4/games",
-    headers: {
-      "Client-ID": "zgvole5aqniz8rnu55rvoxmw3h8d8x",
-      Authorization: "Bearer zzrepo6q4zb5jvw5jxqswmpgc6ef0s",
-      "Content-Type": "application/json",
-    },
-    data: data,
-  };
-  const response = await axios(config);
+  if (searchInput) {
+    const data = `fields name, cover.url, screenshots.url, rating, multiplayer_modes, release_dates.date, platforms.name, genres.name; search "${stringifySearch}";`;
+    const config = {
+      method: "post",
+      url: "https://api.igdb.com/v4/games",
+      headers: {
+        "Client-ID": "zgvole5aqniz8rnu55rvoxmw3h8d8x",
+        Authorization: "Bearer zzrepo6q4zb5jvw5jxqswmpgc6ef0s",
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+    const response = await axios(config);
 
-  // axios(config)
-  //   .then((res) => console.log(res, "1"))
-  //   .catch((err) => console.log(err));
+    // axios(config)
+    //   .then((res) => console.log(res, "1"))
+    //   .catch((err) => console.log(err));
 
-  console.log(response, "2");
+    console.log(response, "2");
 
-  // const results = response.data;
+    // const results = response.data;
 
-  res.json(response.data);
+    res.json(response.data);
+  } else {
+    res.send("Search for your favourite game to see some results");
+  }
 };
 
 module.exports = getGameData;
