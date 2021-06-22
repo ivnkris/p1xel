63a22a78cebc09f8736a51292acee664bf40593f
Tudor Tocan <tudor.tocan@gmail.com>
2021-06-22 22:05:40 +0100

    add code to render steam info to user profile

--- src/controllers/html/renderComparePage.js
+++ src/controllers/html/renderComparePage.js
@@ -1,5 +1,31 @@
-const renderComparePage = (req, res) => {
-  res.render("compare-page");
+const STEAM_API_KEY = "B56468BAD7D8396DF9B20F6148A9080D";
+const axios = require("axios");
+
+const renderComparePage = async (req, res) => {
+  const { profilePicture, steamUsername } = req.session;
+
+  const options = {
+    layout: "main",
+  };
+
+  const getUserAchievements = async () => {
+    const config = {
+      method: "get",
+      url: `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=730&key=${STEAM_API_KEY}&steamid=${steamUsername}`,
+    };
+
+    const response = await axios(config);
+    const results = response;
+    console.log(results);
+    return results;
+  };
+
+  const userData = await getUserAchievements();
+
+  const data = {
+    userData,
+  };
+  res.render("compare-page", { options, data });
 };
 
 module.exports = renderComparePage;
--- src/controllers/html/renderUserProfile.js
+++ src/controllers/html/renderUserProfile.js
@@ -1,44 +1,69 @@
-const STEAM_API_KEY="B56468BAD7D8396DF9B20F6148A9080D"
+const STEAM_API_KEY = "B56468BAD7D8396DF9B20F6148A9080D";
 const axios = require("axios");
 
 const renderUserProfile = async (req, res) => {
-const {profilePicture, steamUsername} = req.session
+  const { profilePicture, steamUsername } = req.session;
 
-const options = {
+  const options = {
     layout: "main",
   };
 
-const getUserData = async () => {
-  const config = {
-    method: "get",
-    url: `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${steamUsername}`, 
+  const getUserData = async () => {
+    const config = {
+      method: "get",
+      url: `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${steamUsername}`,
+    };
+
+    const response = await axios(config);
+    const results = response.data.response.players[0];
+    return results;
   };
-  
-  const response = await axios(config)
-  const results = response.data.response.players[0]
-  return results
-}
 
-const getRecentlyPlayedGames = async () => {
-  const config = {
-    method: "get",
-    url: `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${STEAM_API_KEY}&steamid=${steamUsername}&format=json`, 
+  const getRecentlyPlayedGames = async () => {
+    const config = {
+      method: "get",
+      url: `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${STEAM_API_KEY}&steamid=${steamUsername}&format=json`,
+    };
+
+    const response = await axios(config);
+    const results = response.data.response.games;
+    return results;
   };
-  
-  const response = await axios(config)
-  const results = response.data.response.games
-  return results
-}
 
- const data = {userData: await getUserData(), 
-  userPlayedGames: await getRecentlyPlayedGames()}
+  const getFriendsList = async () => {
+    const config = {
+      method: "get",
+      url: `http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${STEAM_API_KEY}&steamid=${steamUsername}&relationship=friend`,
+    };
 
- console.log(data.userPlayedGames)
+    const response = await axios(config);
+    const results = response.data.friendslist.friends;
 
+    const retrieveFriendsInfo = async (friend) => {
+      const config = {
+        method: "get",
+        url: `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${friend.steamid}`,
+      };
 
-  res.render("user-profile", {options, data});
+      const response = await axios(config);
+      const results = response.data.response.players[0];
+      return results;
+    };
+
+    const friendsProfiles = await Promise.all(results.map(retrieveFriendsInfo));
+    return { friendsProfiles, userFriendsSince: results };
+  };
+
+  const data = {
+    userData: await getUserData(),
+    userPlayedGames: await getRecentlyPlayedGames(),
+    userFriendsList: await getFriendsList(),
+  };
+
+  console.log(data);
+  console.log(data.userFriendsList);
+
+  res.render("user-profile", { options, data });
 };
-
-
 
 module.exports = renderUserProfile;
