5a9aeea812d9f1856e3ef8f2dbd7ba8830148c0c
Lee Farnell <leefarnell@mac.com>
2021-06-23 22:30:28 +0100

    Game cards now load game page and full data for game based on the ID.

--- src/controllers/html/renderGamePage.js
+++ src/controllers/html/renderGamePage.js
@@ -1,5 +1,32 @@
-const renderGamePage = (req, res) => {
-  res.render("games");
+const axios = require("axios");
+
+const renderGamePage = async (req, res) => {
+  const options = {
+    layout: "main",
+  };
+  const { id: gameId } = req.params;
+
+  const data = `fields name, summary, cover.url, screenshots.url, rating, multiplayer_modes, release_dates.date, platforms.name, genres.name; where id =${gameId};`;
+  const config = {
+    method: "post",
+    url: "https://api.igdb.com/v4/games",
+    headers: {
+      "Client-ID": "zgvole5aqniz8rnu55rvoxmw3h8d8x",
+      Authorization: "Bearer zzrepo6q4zb5jvw5jxqswmpgc6ef0s",
+      "Content-Type": "application/json",
+    },
+    data,
+  };
+
+  const response = await axios(config);
+
+  console.log(config);
+
+  const results = response.data;
+
+  const game = results[0];
+
+  return res.render("games", { options, game });
 };
 
 module.exports = renderGamePage;
