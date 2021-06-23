2e0003ebd0a9638839746ccff7a83524fa1a2d82
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-06-23 21:02:48 +0100

    isloggedin team page

--- src/controllers/html/renderTeamPage.js
+++ src/controllers/html/renderTeamPage.js
@@ -1,5 +1,6 @@
 const renderTeamPage = (req, res) => {
-  res.render("team-page");
+  const {isLoggedIn} = req.session.isLoggedIn;
+  res.render("team-page", isLoggedIn);
 };
 
 module.exports = renderTeamPage;
