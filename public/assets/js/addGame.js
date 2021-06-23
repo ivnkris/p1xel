6d7aea3cd9c7886bfd7be6a234c5d18ccf265f43
Lee Farnell <leefarnell@mac.com>
2021-06-23 19:36:24 +0100

    Data returns and moves to database.

--- public/assets/js/addGame.js
+++ public/assets/js/addGame.js
@@ -1,26 +1,22 @@
-
-
 const onSubmit = async (event) => {
   event.preventDefault();
   let want_game;
   let has_game;
 
-  const form  = $(event.currentTarget);
+  const form = $(event.currentTarget);
 
-  
-  const buttonType =  $(document.activeElement).attr("data-type");
+  const buttonType = $(document.activeElement).attr("data-type");
   if (buttonType === "want-to-play") {
     want_game = true;
   } else if (buttonType === "have-played") {
     has_game = true;
-  };
+  }
 
   const name = form.find("#gameName").html();
-  // const cover_art = form.find("#cover").html();
-  // const platform = form.find ("#platform").html();
-  // const critic_rating = form.find ("#criticRating").html();
-  // const release_date = form.find ("#releaseDate").html();
-
+  const cover_art = form.find("#cover").attr("data-type");
+  const platform = form.find("#platform").attr("data-type");
+  const critic_rating = form.find("#criticRating").attr("data-type");
+  const release_date = form.find("#releaseDate").attr("data-type");
 
   const options = {
     method: "POST",
@@ -32,20 +28,18 @@ const onSubmit = async (event) => {
       name,
       want_game,
       has_game,
-      // cover_art,
-      // platform,
-      // critic_rating,
-      // release_date,
+      cover_art,
+      platform,
+      critic_rating,
+      release_date,
     }),
   };
   const response = await fetch(`/api/games/`, options);
   if (response.status !== 200) {
     console.error(error.message);
   } else {
-    console.log("added to my games")
+    console.log("added to my games");
   }
 };
-
-
 
 $('[name="game-form"]').submit(onSubmit);
