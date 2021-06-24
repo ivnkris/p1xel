9ab5b443359f880074b9b100653c505a7563aed5
Tudor Tocan <tudor.tocan@gmail.com>
2021-06-25 00:22:12 +0100

    fixed image on game-card, add code to separate games i have and games i want

--- src/controllers/html/renderUserGames.js
+++ src/controllers/html/renderUserGames.js
@@ -2,16 +2,15 @@ const { Game } = require("../../models");
 
 const renderUserGames = async (req, res) => {
   try {
-    console.log(req.session);
     const { userId } = req.session;
     const games = await Game.findAll({
       where: { user_id: userId },
     });
-    console.log(games);
     const formatGames = games.map((game) => game.get({ plain: true }));
+
     res.render("user-games", { games: formatGames });
   } catch (error) {
-    console.log(error);
+    console.error({ error: "Could not render User Games page." });
   }
 };
 
