6d52c2ce88c54bb3d0cbc0e5a1a1225ecd13c562
Tudor Tocan <tudor.tocan@gmail.com>
2021-06-24 13:18:06 +0100

    add code to render followers list with actual user followers

--- src/controllers/html/renderFollowersList.js
+++ src/controllers/html/renderFollowersList.js
@@ -1,5 +1,31 @@
-const renderFollowersList = (req, res) => {
-  res.render("followers");
+const STEAM_API_KEY = "B56468BAD7D8396DF9B20F6148A9080D";
+const axios = require("axios");
+const { User, Followers } = require("../../models");
+
+const renderFollowersList = async (req, res) => {
+  const { steamUsername, userId } = req.session;
+
+  const options = {
+    layout: "main",
+  };
+
+  const getFollowers = async () => {
+    const followersData = await Followers.findAll({
+      where: {
+        user_id: userId,
+      },
+      include: User,
+    });
+    const formattedFollowersData = followersData.map((follower) =>
+      follower.get({ plain: true })
+    );
+
+    return formattedFollowersData;
+  };
+
+  const data = { userFollowers: await getFollowers() };
+
+  res.render("followers", { options, data });
 };
 
 module.exports = renderFollowersList;
